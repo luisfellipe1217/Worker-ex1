91e7893cc14d044d794b90a7e6acd48a8ea66ff2
luisfellipe1217 <luis.fellipe1217@gmail.com>
2020-08-26 12:06:27 -0300

    Adicionado metodos

--- src/Entities/Worker.java
+++ src/Entities/Worker.java
@@ -2,25 +2,26 @@ package Entities;
 
 import Entities.enums.WorkerLevel;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class Worker {
-    
+
     private String name;
     private WorkerLevel level;
     private Double baseSalary;
-    
+
     private Departament departament;
     private List<HourContract> contracts = new ArrayList<>();
-    
-    public Worker(){
+
+    public Worker() {
     }
 
     public Worker(String name, WorkerLevel level, Double baseSalary, Departament departament) {
         this.name = name;
         this.level = level;
         this.baseSalary = baseSalary;
-        this.departament = departament;        
+        this.departament = departament;
     }
 
     public String getName() {
@@ -59,10 +60,25 @@ public class Worker {
         this.departament = departament;
     }
 
-    public void setContracts(List<HourContract> contracts) {
-        this.contracts = contracts;
+    public void addContract(HourContract contract) {
+        contracts.add(contract);
     }
-    
-    
-    
+
+    public void removeContract(HourContract contract) {
+        contracts.remove(contract);
+    }
+
+    public double income(int year, int month) {
+        double sum = baseSalary;
+        Calendar cal = Calendar.getInstance();
+        for (HourContract c : contracts) {
+            cal.setTime(c.getDate());
+            int c_year = cal.get(Calendar.YEAR);
+            int c_month = 1 + cal.get(Calendar.MONTH);
+            if (year == c_year && month == c_month) {
+                sum += c.totalValue();
+            }
+        }
+        return sum;
+    }
 }
